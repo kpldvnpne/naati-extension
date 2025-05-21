cc0ccffeffa013ea971548e06c5037c01e823a60
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-21 07:42:03 -0400

    Copy to clipboard

--- background.js
+++ background.js
@@ -2,13 +2,38 @@ chrome.contextMenus.onClicked.addListener(genericOnClick);
 
 const menuItemId = 'download-with-ytdlp';
 
+function writeToClipboard(text) {
+  console.debug("Writing to clipboard");
+  navigator.clipboard.writeText(text);
+}
+
+async function copyToClipboard(text, tab) {
+  console.debug("Executing script");
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: writeToClipboard,
+    args: [text],
+  });
+}
+
+async function copyToCurrentTabClipboard(text) {
+  console.debug("Writing to tab");
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  console.debug('Tab id', tab.id);
+  await copyToClipboard(text, tab);
+  console.debug("URL Copied");
+}
+
 function genericOnClick(info) {
   switch (info.menuItemId) {
     case menuItemId:
       console.debug('What we needed: iframe clicked');
       if (info.frameUrl != undefined) {
-        console.debug(`Frame url exists: ${info.frameUrl}`);
-        // TODO: Show the link and copy it to the clipboard.
+        const frameUrl = info.frameUrl;
+        console.debug(`Frame url exists: ${frameUrl}`);
+        // TODO: Show the link
+        const command = `yt-dlp --add-header "Referer: https://www.nepalinaati.com" "${frameUrl}"`
+        copyToCurrentTabClipboard(command);
       }
       break;
     default:
